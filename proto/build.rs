adb7dd7fae6c1d8d85bcaa4111b63fc2f1c98c88
t10471 <t104711202@gmail.com>
2019-08-17 18:35:18 +0900

    add proto rust

---
+++ proto/build.rs
@@ -0,0 +1,17 @@
+extern crate protoc_rust;
+
+use protoc_rust::Customize;
+
+fn main() {
+    protoc_rust::run(protoc_rust::Args {
+        out_dir: "src",
+        input: &["p2p.proto"],
+        includes: &["."],
+        customize: Customize {
+            carllerche_bytes_for_bytes: Some(true),
+            carllerche_bytes_for_string: Some(true),
+            ..Default::default()
+        },
+    })
+    .expect("protoc");
+}
---
+++ proto/src/main.rs
@@ -0,0 +1,42 @@
+extern crate bytes;
+extern crate protobuf;
+mod p2p;
+
+use bytes::Bytes;
+
+use protobuf::*;
+
+fn main() {
+    let m = p2p::Message::new();
+    let buf = Bytes::from(m.write_to_bytes().unwrap());
+    println!("Serialize: {:?}", buf);
+    let msg = parse_from_bytes::<p2p::Message>(&buf).unwrap();
+    println!("Deserialize: {:?}", msg);
+    println!("Type: {:?}", msg.get_field_type());
+
+    let mut m = p2p::Message::new();
+    m.set_field_type(p2p::Message_MessageType::Handshake);
+    m.set_status(p2p::Message_Status::OK);
+    let buf = Bytes::from(m.write_to_bytes().unwrap());
+    println!("Serialize: {:?}", buf);
+    let msg = parse_from_bytes::<p2p::Message>(&buf).unwrap();
+    println!("Deserialize: {:?}", msg);
+    println!("Type: {:?}", msg.get_field_type());
+
+    let mut m = p2p::Message::new();
+    m.set_field_type(p2p::Message_MessageType::Handshake);
+    m.set_status(p2p::Message_Status::NG);
+    let buf = Bytes::from(m.write_to_bytes().unwrap());
+    println!("Serialize: {:?}", buf);
+    let msg = parse_from_bytes::<p2p::Message>(&buf).unwrap();
+    println!("Deserialize: {:?}", msg);
+    println!("Type: {:?}", msg.get_field_type());
+
+    let mut m = p2p::Message::new();
+    m.set_field_type(p2p::Message_MessageType::Bye);
+    let buf = Bytes::from(m.write_to_bytes().unwrap());
+    println!("Serialize: {:?}", buf);
+    let msg = parse_from_bytes::<p2p::Message>(&buf).unwrap();
+    println!("Deserialize: {:?}", msg);
+    println!("Type: {:?}", msg.get_field_type());
+}
